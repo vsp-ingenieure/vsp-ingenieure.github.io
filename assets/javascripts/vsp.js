b74fd4d3bcce3dccf5101bcd4ac6413dd809bc7a
Marcus Exner <marcus.exner@wunderkraut.com>
2014-02-06 00:40:20 +0100

    "Automated commit at 2014-02-05 23:40:20 UTC by middleman-deploy 0.1.4"

---
+++ assets/javascripts/vsp.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function() {
+
+	/*
+	*   Examples - images
+	*/
+	
+	$('#example4').fancybox({
+		transitionIn: 'elastic',
+		transitionOut: 'elastic',
+		speedIn: 600,
+		speedOut: 300
+	});
+});
